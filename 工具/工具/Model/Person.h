01e0c033f50365b489daea9eda10aa67b9b1b57f
yudong123 <2111916870@qq.com>
2018-03-21 12:50:51 +0800

    创建Model文件和Person模型

---
+++ 工具/工具/Model/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  工具
+//
+//  Created by YuDong on 2018/3/21.
+//  Copyright © 2018年 YuDong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ 工具/工具/Model/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  工具
+//
+//  Created by YuDong on 2018/3/21.
+//  Copyright © 2018年 YuDong. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
